93f58098d9d282f6679987b990588f1a8e8d5d5c
gege-projetos <videooneprodutora@gmail.com>
2025-07-28 16:14:36 -0300

    Corrigir erros de build: ordem CSS e dependências @radix-ui

--- vite.config.js
+++ vite.config.js
@@ -20,8 +20,7 @@ export default defineConfig({
 		rollupOptions: {
 			output: {
 				manualChunks: {
-					vendor: ['react', 'react-dom'],
-					ui: ['@radix-ui/react-dialog', '@radix-ui/react-toast']
+					vendor: ['react', 'react-dom']
 				}
 			}
 		}
